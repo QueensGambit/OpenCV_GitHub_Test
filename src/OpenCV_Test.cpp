ffd658e00fa142186528f8d896b684a6f3dd12a1
QueensGambit <curry-berry@freenet.de>
2015-11-22 21:38:03 +0100

    increased brightness

--- src/OpenCV_Test.cpp
+++ src/OpenCV_Test.cpp
@@ -11,8 +11,13 @@ int main()
 
 Mat img = imread("media/Lenna.png", CV_LOAD_IMAGE_COLOR);
 
+Mat imgBright = img + Scalar(50,50,50);
+
 namedWindow("MyWindow", CV_WINDOW_AUTOSIZE);
 imshow("MyWindow", img);
+
+namedWindow("increasedBrightness", CV_WINDOW_AUTOSIZE);
+imshow("increasedBrightness", imgBright);
 
 waitKey(0);
 return 0;

588440b3b563ded45e659bc1fc7f2e9475fb16ff
QueensGambit <curry-berry@freenet.de>
2015-11-22 20:23:37 +0100

    initial push

---
+++ src/OpenCV_Test.cpp
@@ -0,0 +1,20 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+
+Mat img = imread("G:/Gimp/DBV"
+		"/Lenna.png", CV_LOAD_IMAGE_COLOR);
+
+namedWindow("MyWindow", CV_WINDOW_AUTOSIZE);
+imshow("MyWindow", img);
+
+waitKey(0);
+return 0;
+}
